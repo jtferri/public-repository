8628be93bce0d232dec15dc6dcacb143fc4362aa
jtferri <jferri17@gmail.com>
2025-02-27 17:25:59 -0500

    url update

--- sankeywebpage.py
+++ sankeywebpage.py
@@ -1,16 +1,16 @@
 import streamlit as st
-
+import requests
 import streamlit.components.v1 as components
 
 # Set the title of the Streamlit app
 st.title("Sankey Diagram Viewer")
 
-# Path to the HTML file
-html_file_path = "/Users/jacquelineferri/public-repository/sankey.html"
+# URL to the HTML file in the GitHub repository
+html_file_path = "https://github.com/jtferri/public-repository"
 
 # Read the HTML file
 with open(html_file_path, 'r', encoding='utf-8') as html_file:
-    html_content = html_file.read()
+	html_content = html_file.read()
 
 # Display the HTML file in the Streamlit app
-components.html(html_content, height=800)
+components.html(html_content, height=2000, width=2000)
