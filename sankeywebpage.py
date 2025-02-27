222aa84aee29695053c3bee03921af3dd2f8865a
jtferri <jferri17@gmail.com>
2025-02-27 16:54:29 -0500

    update

--- sankeywebpage.py
+++ sankeywebpage.py
@@ -1,7 +1,14 @@
 import streamlit as st
+import os
 
-st.write(""
-         # "DS for PH class 
-         # homework 5
-         ""
-)
+# Display title of webpage
+st.title('Sankey Graphic')
+
+# Load the HTML file
+html_file_path = r"/Users/jacquelineferri/Desktop/data science for public health/DS-for-PH-class-/sankey.html"
+
+# Check if the file exists before displaying it
+if os.path.exists(html_file_path):
+    st.components.v1.html(open(html_file_path).read(), height=600)
+else:
+    st.error("Plot HTML file not found!")
