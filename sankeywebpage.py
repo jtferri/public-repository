1c0e6708812547892effd771bdc752e42fdd7717
jtferri <jferri17@gmail.com>
2025-02-27 17:09:59 -0500

    update

--- sankeywebpage.py
+++ sankeywebpage.py
@@ -4,7 +4,7 @@ import os
 # Display title of webpage
 st.title('Sankey Graphic')
 
-# Path to the HTML file
+# Load the HTML file
 html_file_path = r"/Users/jacquelineferri/public-repository/sankey.html"
 
 # Check if the file exists before displaying it
@@ -12,7 +12,10 @@ if os.path.exists(html_file_path):
     # Open the HTML file and read its contents
     with open(html_file_path, 'r') as file:
         plot_html = file.read()
-    
+
+    # Print the HTML content to the Streamlit console for debugging
+    st.write(plot_html)
+
     # Display the HTML content in Streamlit
     st.components.v1.html(plot_html, height=600)  # Adjust the height as needed
 else:
